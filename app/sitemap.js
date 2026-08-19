d18a10744c86458b9ec362bfc79e928bb2d37774
mauloajin <mauloa32153@gmail.com>
2026-08-19 15:30:45 +0900

    app/sitemap.jsを追加

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://gion-245.vercel.app/",
+      lastModified: new Date("2026-08-19"),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
